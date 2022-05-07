2d171f6da4aeb3d6a7273b7cf956e88b6a65c6c5
Tello- <tellovisionstudio@gmail.com>
2022-05-07 14:40:25 -0700

    Removed back ticks from NOW()

--- SQLScripts/createaddr.sql
+++ SQLScripts/createaddr.sql
@@ -11,14 +11,14 @@ CREATE TABLE IF NOT EXISTS Addresses (
 );
 
 INSERT INTO Addresses(AddressID,ZipCodeID,Time_Stamp,StreetAddress) VALUES
- ('H9GYP6RL',93242,'NOW()','7783 Vine Street')
-,('99jL32w6',93611,'NOW()','12367 Adams St')
-,('U7bNwtID',93309,'NOW()','1237 W Eymann Ave')
-,('jjSwnj9T',91340,'NOW()','1238 Lonestar Dr')
-,('9rQCnJiz',91750,'NOW()','1239 Ashby Ave')
-,('R6YJS6Po',91016,'NOW()','12388 Loraine Ave')
-,('XGLam0dZ',93901,'NOW()','12373 Windchime Pl')
-,('Aad9BPEx',93907,'NOW()','1237 E 24th St')
-,('Ir9hq0KU',93926,'NOW()','1237 N Lowery St')
-,('088qGXhH',92704,'NOW()','12376 5th St')
-,('5QQ13378',92806,'NOW()','1239 Ashby Ave');
+ ('H9GYP6RL',93242,NOW(),'7783 Vine Street')
+,('99jL32w6',93611,NOW(),'12367 Adams St')
+,('U7bNwtID',93309,NOW(),'1237 W Eymann Ave')
+,('jjSwnj9T',91340,NOW(),'1238 Lonestar Dr')
+,('9rQCnJiz',91750,NOW(),'1239 Ashby Ave')
+,('R6YJS6Po',91016,NOW(),'12388 Loraine Ave')
+,('XGLam0dZ',93901,NOW(),'12373 Windchime Pl')
+,('Aad9BPEx',93907,NOW(),'1237 E 24th St')
+,('Ir9hq0KU',93926,NOW(),'1237 N Lowery St')
+,('088qGXhH',92704,NOW(),'12376 5th St')
+,('5QQ13378',92806,NOW(),'1239 Ashby Ave');
