db97dff774ce1701ece7767ef05d17edbdf39647
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2022-05-08 21:28:05 -0700

    Added required form fields for user registration

--- htdocs/auth/register.php
+++ htdocs/auth/register.php
@@ -101,19 +101,19 @@ function printRegisterForm($error = "") {
         <form action="register.php" method="post">
             <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" required>
             </div>
             <div class="mb-3">
                 <label for="firstname" class="form-label">First Name</label>
-                <input type="text" class="form-control" id="firstname" name="firstname" aria-describedby="emailHelp">
+                <input type="text" class="form-control" id="firstname" name="firstname" aria-describedby="emailHelp" required>
             </div>
             <div class="mb-3">
                 <label for="lastname" class="form-label">Last Name</label>
-                <input type="text" class="form-control" id="lastname" name="lastname" aria-describedby="emailHelp">
+                <input type="text" class="form-control" id="lastname" name="lastname" aria-describedby="emailHelp" required>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password">
+                <input type="password" class="form-control" id="password" name="password" required>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
