74490aeb4d67c6e8b65ffab5bf19f2c08499fccf
Gabriel Warkentin <gabriel.warkentin@gmail.com>
2022-05-15 11:01:32 -0700

    function to get all posts by employer

--- htdocs/search/index.php
+++ htdocs/search/index.php
@@ -9,6 +9,39 @@ $inject = [
     'body' => ''
 ];
 
+if (isset($_GET['employerid'])) {
+    [$error, $posts] = getEmployerPosts($_GET['employerid']);
+}
+else {
+    [$error, $posts] = getAllPosts();
+}
+
+if (isset($posts)) {
+    $inject['body'] = printPosts($posts);
+}
+else {
+    $inject['warning'] = 'Failed to fetch job posts. ' . $error;
+}
+
+printMain($inject);
+
+function getEmployerPosts($employerid) {
+    try {
+        $stmt = $GLOBALS['conn']->prepare("SELECT * FROM PostPreviewView WHERE EmployerID = ? LIMIT 50");
+        $stmt->bind_param('i', $employerid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $jobposts = $result->fetch_all(MYSQLI_ASSOC);
+        if (isset($jobposts)) {
+            return [NULL, $jobposts];
+        }
+        return ['Failed to find JobPosts.', NULL];
+    }
+    catch (Exception $e) {
+        return ['Failed to find JobPosts. ' . $e, NULL];
+    }
+}
+
 function getAllPosts() {
     try {
         $result = $GLOBALS['conn']->query("SELECT * FROM PostPreviewView LIMIT 50");
@@ -22,15 +55,4 @@ function getAllPosts() {
         return ['Failed to find JobPosts. ' . $e, NULL];
     }
 }
-
-[$error, $posts] = getAllPosts();
-if (isset($posts)) {
-    $inject['body'] = printPosts($posts);
-}
-else {
-    $inject['warning'] = 'Failed to fetch job posts. ' . $error;
-}
-
-printMain($inject);
-
 ?>
